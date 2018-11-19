ce19f0045291a82ee009f8efd60066200a8a9d7c
elfein <elfein99@gmail.com>
2018-11-19 12:53:02 -0500

    Style Single Artist page

--- client/src/components/SingleArtist.js
+++ client/src/components/SingleArtist.js
@@ -1,6 +1,61 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+max-width: 1200px;
+margin: 0 auto;
+h1, h3 {
+    font-weight: 200;
+}
+a {
+    text-decoration: none;
+    border-bottom: 3px double #000;
+    color: #000;
+    padding: 0 0 16px 0;
+    margin: 0 0 16px 0;
+    &:hover {
+        color: rgb(245,240, 200);
+    }
+}
+img {
+    width: 100%;
+    max-width: 600px;
+}
+.container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+.song {
+    &:hover {
+        transform: rotate(5deg);
+        transition: transform ease 0.4s;
+    }
+    transform: rotate(0deg);
+        transition: transform ease 0.4s;
+    background: rgb(245,240, 200);
+    margin: 10px;
+    padding: 20px;
+    border-radius: 5px;
+    text-align: center;
+    max-width: 300px;
+    h4 {
+        font-weight: 200;
+        text-transform: uppercase;
+        letter-spacing: 0.2em;
+        padding: 0 0 16px 0;
+        border-bottom: 3px double #000;
+    }
+}
+audio {
+    width: 80px;
+}
+`
 
 export default class SingleArtist extends Component {
     state = {
@@ -30,23 +85,25 @@ export default class SingleArtist extends Component {
 
         const songContent = this.state.songs.map((song, i) => {
             return (
-                <div key={i}>
+                <div className="song" key={i}>
                     <h4>{song.title} – {song.album}</h4>
-                {song.preview_url ?
-                    <audio controls src={song.preview_url}></audio> :
-                    <h4>No audio content.</h4> }
+                    {song.preview_url ?
+                        <audio controls src={song.preview_url}></audio> :
+                        <h4>No audio content.</h4>}
                 </div>
             )
         })
 
         return (
-            <div>
+            <StyledDiv>
                 <h1>{artist.name} ({artist.nationality})</h1>
-                <div><img alt={artist.name} width={200} src={artist.photo_url} /></div>
+                <div><img alt={artist.name} src={artist.photo_url} /></div>
                 <h3>ID: {this.props.match.params.id}</h3>
                 <Link to='/artists'>Go Back to All Artists</Link>
-                {songContent}
-            </div>
+                <div className="container" >
+                    {songContent}
+                </div>
+            </StyledDiv>
         )
     }
 }
